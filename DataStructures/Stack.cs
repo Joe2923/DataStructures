800006cd5aae641d2da488c4899cc0ae20790af4
Joseph Nelson <15NelsonJ@wg6.co.uk>
2020-09-22 15:07:45 +0100

    stack constructor

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Stack
+    {
+        int[] stack;
+        int sp;
+        int max;
+
+        public Stack(int size)
+        {
+            max = size;
+            stack = new int[max];
+            sp = 0;
+        }
+
+        public void Push(int item)
+        {
+            
+        }
+        public int Pop()
+        {
+            if( sp > 0 )
+            {
+                sp--;
+                return stack[sp];
+            }
+            throw new Exception("STACK EMPTY");
+            
+        }
+    }
+}
